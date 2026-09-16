98dbcf9e7ee1f40a170b9b0eb08b8c026951613c
lotgomez <lotjgomez@gmail.com>
2026-09-16 16:31:32 -0300

    vr 2

--- skin_vr.js
+++ skin_vr.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - VR - Skin
 // Pano2VR 8.0.5/22607
 // Filename: feather_vr.ggsk
-// Generated 2026-09-16T19:30:13Z
+// Generated 2026-09-16T19:31:18Z
 
 function pano2vrVrSkin(player,base) {
 	player.addVariable('node_cloner_vr_hasUp', 2, false, { ignoreInState: 0  });
